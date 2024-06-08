3ccfb8f9c904af7defc0259e14358d2c63d9d6b5
Armando <borelliarmando28@gmail.com>
2024-06-07 21:33:35 -0400

    Added User model file

---
+++ PaperTrader/Models/User.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PaperTrader.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt { get; set; }
+        [DataType(DataType.Date)]
+    public DateTime UpdatedAt { get; set; }
+}
