07fcfea3dd27d174bc342f6520453d49432cba98
Armando <borelliarmando28@gmail.com>
2024-06-07 21:53:17 -0400

    Removed updated_at from user model

--- PaperTrader/Models/User.cs
+++ PaperTrader/Models/User.cs
@@ -8,7 +8,5 @@ public class User
     public string? Username { get; set; }
     public string? Password { get; set; }
     [DataType(DataType.Date)]
-    public DateTime CreatedAt { get; set; }
-        [DataType(DataType.Date)]
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
 }
