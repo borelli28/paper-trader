b3d0bebaac99fd0047403212e77ff6cd2a7f21b7
Armando <borelliarmando28@gmail.com>
2024-06-09 23:04:12 -0400

    Removed privacy method in App controller

--- PaperTrader/Controllers/AppController.cs
+++ PaperTrader/Controllers/AppController.cs
@@ -19,11 +19,6 @@ public class AppController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
