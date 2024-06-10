ec6d2ef7e2239b6afd8e0acf814f65d4fde1c929
Armando <borelliarmando28@gmail.com>
2024-06-09 20:59:37 -0400

    Changed User Register method to redirect to Login page after sucess

--- PaperTrader/Controllers/UserController.cs
+++ PaperTrader/Controllers/UserController.cs
@@ -57,7 +57,7 @@ namespace PaperTrader.Controllers
             {
                 _context.Add(user);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Login", "User");
             }
             return View(user);
         }
