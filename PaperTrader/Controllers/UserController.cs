1275fca367ecaabb0f76b79d9f16ab9c46a8d7b8
Armando <borelliarmando28@gmail.com>
2024-06-09 20:56:50 -0400

    Renamed User/Create to User/Register

--- PaperTrader/Controllers/UserController.cs
+++ PaperTrader/Controllers/UserController.cs
@@ -44,18 +44,14 @@ namespace PaperTrader.Controllers
             return View(user);
         }
 
-        // GET: User/Create
-        public IActionResult Create()
+        public IActionResult Register()
         {
             return View();
         }
 
-        // POST: User/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Username,Password,CreatedAt")] User user)
+        public async Task<IActionResult> Register([Bind("Id,Username,Password,CreatedAt")] User user)
         {
             if (ModelState.IsValid)
             {
